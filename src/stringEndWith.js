c214976e820763934f1706df9551343f2ed7fb69
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-06-26 09:32:49 +0200

    Refactor after 6th test passed

--- src/stringEndWith.js
+++ src/stringEndWith.js
@@ -3,14 +3,14 @@ function stringEndWith(str1 ='', str2='') {
   str1Length = str1.length;
   str2Length = str2.length;
 
-  console.log('Str1 end: ',str1.slice(-str2Length))
 
-  if (str2Length === 0 && str1Length > 0 ) return false
+  if (str2Length === 0 && str1Length > 0 ) return false;
   if (str2Length > str1Length) return false;
-  if (str1 === "ababac" && str2 === "ab") return false
 
 
-  return true;
+  
+  if (str1.slice(-str2Length) === str2) return true
+  else return false;
 }
 
 module.exports = stringEndWith;
