df63ba47e666b1f18dff16fa820133262d71bbd7
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-16 22:55:54 +0200

    Adding TS solution

--- src/stringEndWith.js
+++ src/stringEndWith.js
@@ -7,7 +7,10 @@ function stringEndWith(str1 ='', str2='') {
   if (str1Length === 0 ||  str2Length > str1Length) return false;
 
   return str1.slice(-str2Length) === str2;
-
 }
+
+// export function solution(str: string, ending: string): boolean { 
+//   return str.slice((str.length - ending.length)) === ending;
+// }
 
 module.exports = stringEndWith;
